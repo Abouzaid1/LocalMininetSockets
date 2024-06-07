20ea2e79b80670275fdd751126faf23d0d6a9770
Your Name <am5715073@gmail.com>
2024-06-07 17:19:23 +0300

    abouzaid

---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+def install_node_python():
+    try:
+        subprocess.run(['npm,', 'install'], check=True)
+        subprocess.run(['npm,', 'start', 'index'], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+        print("Please run this script with sudo privileges.")
+
+if __name__ == "__main__":
+    install_node_python()
