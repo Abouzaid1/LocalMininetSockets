b01865d439764e0ba04c87b399538efd17c7711a
Your Name <am5715073@gmail.com>
2024-06-08 02:40:28 +0300

    abouzaid

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const http = require('http');
-const { createServer } = require('node:http');
-const { Server } = require("socket.io");
 const express = require('express');
+const { Server } = require('socket.io');
+const cors = require('cors')
+// Create an Express application
 const app = express();
+app.use(cors())
+// Create an HTTP server and pass the Express app as a handler
 const server = http.createServer(app);
-const io = new Server(server)
+
+// Create a new Socket.IO server and attach it to the HTTP server
+const io = new Server(server);
+
+// Listen for incoming connections on the Socket.IO server
 io.on('connect', (socket) => {
+    console.log('User connected');
+
+    // Handle the 'joinTopo' event from clients
     socket.on('joinTopo', (data) => {
-        console.log('User Joined');
-        socket.emit("joinTopo",data)
+        console.log('User joined with data:', data);
+
+        // Emit the 'joinTopo' event back to the client with the received data
+        io.emit('send', "hello");
+    });
+
+    // Handle client disconnect
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
     });
 });
 
-server.listen(6000, '0.0.0.0', () => {
-    console.log('server running at 6000');
+// Start the server and listen on port 6000, accessible from any network interface
+server.listen(8080, '0.0.0.0', () => {
+    console.log('Server running at http://0.0.0.0:6000');
 });
